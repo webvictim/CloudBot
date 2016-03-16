c72f78da1b6167b5b4615aefb803bb3930687fd4
webvictim <webvictim@gmail.com>
2016-03-16 15:15:34 +0000

    Changed inefficient shrugging

--- plugins/shrug.py
+++ plugins/shrug.py
@@ -2,12 +2,7 @@ from cloudbot import hook
 
 shrugdude = u'\u00AF\_(\u30C4)_/\u00AF'
 
-@hook.command(autohelp=False)
+@hook.command('shrug', 'supson', autohelp=False)
 def shrug(message, conn):
-    """shrug yo"""
-    message(shrugdude)
-
-@hook.command(autohelp=False)
-def supson(message, conn):
     """SUP SON"""
     message(shrugdude)
