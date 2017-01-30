34b1dfc5cbc1620156faca084fa0cd66b1c583af
webvictim <webvictim@gmail.com>
2017-01-30 01:24:13 +0000

    Re-added location saving to improvd weather lookup code

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,12 +1,24 @@
-import requests
 import re
+import requests
 
+from sqlalchemy import Table, Column, PrimaryKeyConstraint, String
+from sqlalchemy.sql import select
 from cloudbot import hook
-from cloudbot.util import web
+from cloudbot.util import web, database
 
 
 class APIError(Exception):
     pass
+
+# Define database table
+
+table = Table(
+    "weather",
+    database.metadata,
+    Column('nick', String),
+    Column('loc', String),
+    PrimaryKeyConstraint('nick')
+)
 
 # Define some constants
 google_base = 'https://maps.googleapis.com/maps/api/'
@@ -38,6 +50,7 @@ def check_status(status):
         return None
 
 
+
 def find_location(location):
     """
     Takes a location as a string, and returns a dict of data
@@ -56,37 +69,76 @@ def find_location(location):
     error = check_status(json['status'])
     if error:
         raise APIError(error)
+    
+    formatted_address = json['results'][0]['formatted_address']
+    if formatted_address:
+        if formatted_address.endswith(", USA"):
+            return formatted_address.replace(", USA", "")
 
     first_result = json['results'][0]
-    formatted_address = first_result['formatted_address']
-    if formatted_address:
-        # The ", USA" in e.g. "Chicago, AL, USA" confuses wunderground
-        if formatted_address.endswith(", USA"):
-            formatted_address = formatted_address.replace(", USA", "")
-        return formatted_address
-
     location_data = first_result['geometry']['location']
     return "{lat},{lng}".format(**location_data)
 
 
+
+def load_cache(db):
+    global location_cache
+    location_cache = []
+    for row in db.execute(table.select()):
+        nick = row["nick"]
+        location = row["loc"]
+        location_cache.append((nick,location))
+
+def add_location(nick, location, db):
+    test = dict(location_cache)
+    location = str(location)
+    if nick.lower() in test:
+        db.execute(table.update().values(loc=location.lower()).where(table.c.nick == nick.lower()))
+        db.commit()
+        load_cache(db)
+    else:
+        db.execute(table.insert().values(nick=nick.lower(), loc=location.lower()))
+        db.commit()
+        load_cache(db)
+
 @hook.on_start
-def on_start(bot):
+def on_start(bot, db):
     """ Loads API keys """
     global dev_key, wunder_key
     dev_key = bot.config.get("api_keys", {}).get("google_dev_key", None)
     wunder_key = bot.config.get("api_keys", {}).get("wunderground", None)
+    load_cache(db)
 
 
-@hook.command("weather", "we")
-def weather(text, reply):
+def get_location(nick):
+    """looks in location_cache for a saved location"""
+    location = [row[1] for row in location_cache if nick.lower() == row[0]]
+    if not location:
+        return
+    else:
+        location = location[0]
+    return location
+
+@hook.command("weather", "we", autohelp=False)
+def weather(text, reply, db, nick, notice):
     """weather <location> -- Gets weather data for <location>."""
     if not wunder_key:
         return "This command requires a Weather Underground API key."
     if not dev_key:
         return "This command requires a Google Developers Console API key."
 
+    location = ""
+    # If no input try the db
+    if not text:
+        location = get_location(nick)
+        if not location:
+            notice(weather.__doc__)
+            return
+    else:
+        location = text
+    # use find_location to get location data from the user input
     try:
-        formatted_location = find_location(text)
+        formatted_location = find_location(location)
     except APIError as e:
         return e
 
@@ -123,12 +175,22 @@ def weather(text, reply):
 
     # Get the more accurate URL if available, if not, get the generic one.
     if "?query=," in response["current_observation"]['ob_url']:
-        weather_data['url'] = web.shorten(response["current_observation"]['forecast_url'])
+        try:
+            weather_data['url'] = web.try_shorten(response["current_observation"]['forecast_url'])
+        except:
+            weather_data['url'] = response["current_observation"]["forecast_url"]
+            pass
     else:
-        weather_data['url'] = web.shorten(response["current_observation"]['ob_url'])
+        try:
+            weather_data['url'] = web.try_shorten(response["current_observation"]['ob_url'])
+        except:
+            weather_data['url'] = response["current_observation"]["ob_url"]
+            pass
 
     reply("{place} - \x02Current:\x02 {conditions}, {temp_f}F/{temp_c}C, {humidity}, "
           "Wind: {wind_mph}MPH/{wind_kph}KPH {wind_direction}, \x02Today:\x02 {today_conditions}, "
           "High: {today_high_f}F/{today_high_c}C, Low: {today_low_f}F/{today_low_c}C. "
           "\x02Tomorrow:\x02 {tomorrow_conditions}, High: {tomorrow_high_f}F/{tomorrow_high_c}C, "
           "Low: {tomorrow_low_f}F/{tomorrow_low_c}C - {url}".format(**weather_data))
+    if text:
+        add_location(nick, location, db)
