af7fecd14683949b1dd75f42a8e7bb684727ee85
webvictim <webvictim@gmail.com>
2017-03-20 12:24:57 +0000

    New stock module

--- plugins/stock.py
+++ plugins/stock.py
@@ -1,55 +1,39 @@
-from urllib.parse import quote_plus
-
+import csv
 import requests
 
 from cloudbot import hook
 
-BASE_URL = "http://query.yahooapis.com/v1/public/yql"
-ENV = "http://datatables.org/alltables.env"
+# See http://www.jarloo.com/yahoo_finance/ for field names.
+field_names = ['symbol', 'name', 'ask', 'change', 'percent_change']
+query_url = "http://finance.yahoo.com/d/quotes.csv?f=snac1p2"
+link_url = "https://finance.yahoo.com/quote/"
 
 
-def get_data(symbol):
-    query = 'SELECT * FROM yahoo.finance.quote WHERE symbol="{}" LIMIT 1'.format(quote_plus(symbol))
-    request = requests.get(BASE_URL, params={'q': query, 'env': ENV, 'format': 'json'})
+def get_ticker_data(symbol):
+    request = requests.get(query_url, params={'s': symbol})
     request.raise_for_status()
 
-    return request.json()['query']
+    result = next(csv.DictReader([request.text], fieldnames=field_names))
+
+    if result['name'] == "N/A":
+        return None
+
+    return result
 
 
 @hook.command()
 def stock(text):
     """<symbol> -- gets stock information"""
-    sym = text.strip().lower()
+    symbol = text.strip().upper()
 
     try:
-        data = get_data(text)
+        data = get_ticker_data(symbol)
     except requests.exceptions.HTTPError as e:
         return "Could not get stock data: {}".format(e)
 
-    if not data["results"]:
-        return "No results."
+    if not data:
+        return "No results for that symbol."
 
-    quote = data['results']['quote']
+    data['color'] = "04" if float(data['change']) < 0 else "03"
 
-    # if we don't get a company name back, the symbol doesn't match a company
-    if quote['Change'] is None:
-        return "Unknown ticker symbol: {}".format(sym)
-
-    change = float(quote['Change'])
-    price = float(quote['LastTradePriceOnly'])
-
-    # this is for dead companies, if this isn't here PercentChange will fail with DBZ
-    if price == 0 and change == 0:
-        return "\x02{Name}\x02 (\x02{symbol}\x02) - {LastTradePriceOnly}".format(**quote)
-
-    if change < 0:
-        quote['color'] = "5"
-    else:
-        quote['color'] = "3"
-
-    quote['PercentChange'] = 100 * change / (price - change)
-
-    return "\x02{Name}\x02 (\x02{symbol}\x02): {LastTradePriceOnly} " \
-           "\x03{color}{Change} ({PercentChange:.2f}%)\x03 " \
-           "- Day Range: {DaysRange} " \
-           "MCAP: {MarketCapitalization}".format(**quote)
+    return "{symbol} ({name}): {ask} \x03{color}{change} ({percent_change})\x03 - ".format(**data) + link_url + symbol
